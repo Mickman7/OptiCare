4f1dc4a3fb3f323aa6d76b8cb71b87178eb73f47
Michael <michaeljohn_charles@yahoo.com>
2025-04-23 08:22:46 +0100

    patient profile form style updates

--- OptiCare/src/screens/PatientProfile.js
+++ OptiCare/src/screens/PatientProfile.js
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Text, View, ActivityIndicator, TextInput } from 'react-native'
+import { Image, StyleSheet, Text, View, ActivityIndicator, TextInput, ScrollView } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { FIREBASE_APP, FIREBASE_DB } from '../../FirebaseConfig';
 import { doc, getDoc, addDoc, collection } from "firebase/firestore";
@@ -56,38 +56,125 @@ const PatientProfile = ({route}) => {
     //   }, [userData])
 
       if (loading) {
-        return <ActivityIndicator size="large" color="#0000ff" />;
+        return <ActivityIndicator size="large" color="#0000ff" style={styles.loader} />;
       }
 
   return (
-    <View>
-        <View>
-            {/* <Image source={require()}/> */}
-            <Text>First Name: {userData.firstName}</Text>
-            <Text>Last Name: {userData.lastName}</Text>
-            <Text>Date of Birth:{}</Text>
-            <Text>Gender: {userData.gender}</Text>
-            <Text>NHS Number: {userData.nhsNum}</Text>
+    <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.profileHeader}>
+            <Image 
+                source={{ uri: 'https://via.placeholder.com/150' }} 
+                style={styles.profileImage} 
+            />
+            <Text style={styles.profileName}>{userData.firstName} {userData.lastName}</Text>
         </View>
-        <Text>Email: {userData.email}</Text>
-        <Text>Email: {userData.telephone}</Text>
-
-        <View>
-            <Text>Notes:</Text>
+        <View style={styles.infoSection}>
+            <Text style={styles.infoLabel}>Date of Birth:</Text>
+            <Text style={styles.infoValue}>{userData.dob || 'N/A'}</Text>
+            <Text style={styles.infoLabel}>Gender:</Text>
+            <Text style={styles.infoValue}>{userData.gender}</Text>
+            <Text style={styles.infoLabel}>NHS Number:</Text>
+            <Text style={styles.infoValue}>{userData.nhsNum}</Text>
+            <Text style={styles.infoLabel}>Email:</Text>
+            <Text style={styles.infoValue}>{userData.email}</Text>
+            <Text style={styles.infoLabel}>Telephone:</Text>
+            <Text style={styles.infoValue}>{userData.telephone}</Text>
+        </View>
+        <View style={styles.notesSection}>
+            <Text style={styles.notesLabel}>Notes:</Text>
             <TextInput 
                 value={notes}
                 onChangeText={setNotes}
-                style={{width: 300, height: 100, borderWidth: 1}}
+                style={styles.notesInput}
+                multiline
+                placeholder="Add your notes here..."
             />
         </View>
         <AuthForm.AuthButton
             label='Add Note'
             onPress={addNotes}
+            style={styles.addButton}
         />
-    </View>
+    </ScrollView>
   )
 }
 
-export default PatientProfile
+const styles = StyleSheet.create({
+    container: {
+        flexGrow: 1,
+        padding: 20,
+        backgroundColor: '#f9f9f9',
+        alignItems: 'center'
+    },
+    loader: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    profileHeader: {
+        alignItems: 'center',
+        marginBottom: 20,
+        width: '100%'
+    },
+    profileImage: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        marginBottom: 10,
+    },
+    profileName: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#333',
+    },
+    infoSection: {
+        marginBottom: 20,
+        backgroundColor: '#fff',
+        padding: 15,
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+        elevation: 3,
+        width: '100%'
 
-const styles = StyleSheet.create({})
+    },
+    infoLabel: {
+        fontSize: 16,
+        fontWeight: '600',
+        color: '#555',
+    },
+    infoValue: {
+        fontSize: 16,
+        color: '#333',
+        marginBottom: 10,
+    },
+    notesSection: {
+        marginBottom: 20,
+        width: '100%'
+
+    },
+    notesLabel: {
+        fontSize: 18,
+        fontWeight: '600',
+        color: '#555',
+        marginBottom: 10,
+    },
+    notesInput: {
+        width: '100%',
+        height: 100,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 10,
+        padding: 10,
+        backgroundColor: '#fff',
+        textAlignVertical: 'top',
+        
+    },
+    addButton: {
+        marginTop: 10,
+        alignSelf: 'center',
+    },
+});
+
+export default PatientProfile
